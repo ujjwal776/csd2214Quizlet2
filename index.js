b7ab165d358b1dad3ff95074ec4ce22486b71622
Ujjwal Preet <749089@student.tor.loc>
2019-06-26 15:01:38 -0400

    3pm commit

--- index.js
+++ index.js
@@ -2,4 +2,17 @@ function waitingfn(timeinms) {
     const future = future + timeinms;
 
 
+    while (future > Date.now()) {
+
+
+    }
+
+
 }
+
+
+waitingfn(3000);
+
+
+debugger;
+console.log("function about to end here");
